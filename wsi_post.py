b02ed09b166dd58fe03cf31721c4c477e4dd533f
Pacific89 <philipp.faulhammer@medunigraz.at>
2022-02-21 14:47:46 +0100

    adapt patch size

--- wsi_post.py
+++ wsi_post.py
@@ -9,6 +9,7 @@ import scipy.io as sio
 import cv2
 import json
 import openslide
+from skimage.measure import label, regionprops
 
 from misc.wsi_handler import get_file_handler
 from misc.viz_utils import visualize_instances_dict
@@ -16,21 +17,47 @@ from misc.viz_utils import visualize_instances_dict
 
 # wsi_path = "/home/user/Documents/Master/tcga_test/gdc_download_20211215_084557.672401/9baa25bd-c9d1-4280-bc17-43b90fafd4e0/three.svs"
 
-wsi_path = "/home/user/Documents/Master/data/one/one.svs"
+wsi_path = "/media/user/easystore/HRD-Subset/DigitalSlide_A1M_2S_1_20190127153640117/DigitalSlide_A1M_2S_1_20190127153640117.svs"
 
-wsi_basename = "one"
-wsi_json_path = "/home/user/Documents/Master/server_hover_recieve/one/"
+wsi_basename = wsi_path.split("/")[-1].split(".svs")[0]
+print(wsi_basename)
+wsi_json_path = "/media/user/easystore/HRD-Subset/DigitalSlide_A1M_2S_1_20190127153640117/results/d7ca4f688ae04bad9627b5b14956881d"
 
 wsi_one = openslide.open_slide(wsi_path)
 print(wsi_one.dimensions)
 
 
-
-mask_path_wsi = wsi_json_path + 'mask/' + wsi_basename + '.png'
-thumb_path_wsi = wsi_json_path + 'thumb/' + wsi_basename + '.png'
+wsi_png = wsi_basename + ".png"
+mask_path_wsi = os.path.join(wsi_json_path, 'mask', wsi_png)
+thumb_path_wsi = os.path.join(wsi_json_path, 'thumb', wsi_basename + '.png')
 
 thumb = cv2.cvtColor(cv2.imread(thumb_path_wsi), cv2.COLOR_BGR2RGB)
 mask = cv2.cvtColor(cv2.imread(mask_path_wsi), cv2.COLOR_BGR2RGB)
+
+label_mask = label(mask)
+props = regionprops(label_mask)
+
+areas = []
+for prop in props:
+    areas.append(prop.area)
+
+# get largest object
+max_prop = props[np.argmax(areas)]
+bbox = max_prop.bbox
+print(bbox)
+
+top_left = [bbox[0], bbox[1]]
+bot_right = [bbox[3], bbox[4]]
+
+y_mask_ratio = top_left[0] / mask.shape[0]
+y_original = int(wsi_one.dimensions[1]*y_mask_ratio)
+
+y_original += 15000
+
+x_mask_ratio = top_left[1] / mask.shape[1]
+x_original = int(wsi_one.dimensions[0]*x_mask_ratio)
+
+x_original += 16000
 
 # plot the low resolution thumbnail along with the tissue mask
 
@@ -49,12 +76,13 @@ mask = cv2.cvtColor(cv2.imread(mask_path_wsi), cv2.COLOR_BGR2RGB)
 # plt.show()
 
 
-json_path_wsi = wsi_json_path + 'json/' + wsi_basename + '.json'
+json_path_wsi = os.path.join(wsi_json_path, 'json', wsi_basename + '.json')
 
 bbox_list_wsi = []
 centroid_list_wsi = []
 contour_list_wsi = [] 
 type_list_wsi = []
+patch_size = 1000
 
 # add results to individual lists
 with open(json_path_wsi) as json_file:
@@ -64,24 +92,25 @@ with open(json_path_wsi) as json_file:
     for inst in nuc_info:
         inst_info = nuc_info[inst]
         inst_centroid = inst_info['centroid']
-        centroid_list_wsi.append(inst_centroid)
-        inst_contour = inst_info['contour']
-        contour_list_wsi.append(inst_contour)
-        inst_bbox = inst_info['bbox']
-        bbox_list_wsi.append(inst_bbox)
-        inst_type = inst_info['type']
-        type_list_wsi.append(inst_type)
+        if inst_centroid[0] > x_original and inst_centroid[1] > y_original and inst_centroid[0] < x_original+patch_size and inst_centroid[1] < y_original+patch_size:
+            centroid_list_wsi.append(inst_centroid)
+            inst_contour = inst_info['contour']
+            contour_list_wsi.append(inst_contour)
+            inst_bbox = inst_info['bbox']
+            bbox_list_wsi.append(inst_bbox)
+            inst_type = inst_info['type']
+            type_list_wsi.append(inst_type)
 
 
-keys = nuc_info.keys()
-print(nuc_info[list(keys)[-1]])
+# keys = nuc_info.keys()
+print("Kept Nuclei: ", len(centroid_list_wsi))
 print(mag_info)
 
 # define the region to select
-x_tile = 4000
-y_tile = 3000
-w_tile = 15000
-h_tile = 20000
+x_tile = x_original
+y_tile = y_original
+w_tile = patch_size
+h_tile = patch_size
 
 coords = (x_tile, y_tile)
 patch_level = -1
@@ -123,7 +152,7 @@ type_info = {
 }
 
 fig = plt.figure(figsize=(100,80))
-overlaid_output = visualize_instances_dict(wsi_tile, tile_info_dict, type_colour=type_info, line_thickness=3)
+overlaid_output = visualize_instances_dict(wsi_tile, tile_info_dict, type_colour=type_info, line_thickness=2)
 plt.imshow(overlaid_output)
 plt.axis('off')
 plt.title('Segmentation Overlay')
@@ -134,5 +163,9 @@ for i in type_info:
     plt.plot([0,0], label = label, color=tuple(color))
 
 plt.legend()
-plt.savefig("hovernet_line2_dpi.jpg", dpi='figure')
-# plt.show()
+# plt.savefig("hovernet_line2_dpi.jpg", dpi='figure')
+fig2 = plt.figure(figsize=(100,80))
+plt.imshow(wsi_tile)
+plt.axis('off')
+plt.title('Original')
+plt.show()
